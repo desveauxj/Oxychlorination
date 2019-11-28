eaeee762b5b0a2de7e1aa6c3d668ea342a2eefd0
desveauxj <jsd2498@gmail.com>
2019-11-28 10:25:52 -0700

    Add files via upload
    Implemented Ergun! Also cleaned up code a bit.

--- handler.m
+++ handler.m
@@ -1,4 +1,4 @@
-function outvar = handler(v,y,phi,H_tot,Cp_tot,L,D,Beta,Dp,Ac,N,U,flowC,Ftotal_0,T0,P0,rho0,dv)
+function outvar = handler(v,y,phi,H_tot,Cp_tot,L,D,Beta,Ac,U,flowC,Ftotal_0,T0,P0,rho0)
 %Species indices key:
     % 1 = c2h4
     % 2 = hcl
@@ -93,7 +93,7 @@ outvar(9) = (-term1 - term2) / term3; % units of K/m^3
 
 % Ergun differetial equation
 rho = rho0 * (P/P0) * (Ftotal_0/Ftotal) * (T0/T); % units of kg/m^3
-outvar(10) = Beta * (1/(Ac*rho)); % units of kPa/m^3
+outvar(10) = -Beta * (1/(Ac*rho)); % units of kPa/m^3
 
 
 % Shell side differential equations
--- main.m
+++ main.m
@@ -25,26 +25,26 @@ Cp_5 = 0.0418;
 Cp_6 = 0.0351;
 Cp_7 = 0.0937;
 Cp_8 = 0.0345;
-sumCp = Cp_1 + Cp_2 + Cp_3 + Cp_4 + Cp_5 + Cp_6 + Cp_7 + Cp_8;
-Cp_tot = [Cp_1, Cp_2, Cp_3, Cp_4, Cp_5, Cp_6, Cp_7, Cp_8]; 
+Cp_tot = [Cp_1 Cp_2 Cp_3 Cp_4 Cp_5 Cp_6 Cp_7 Cp_8];
+sumCp = sum(Cp_tot);
+ 
 
 %Main Properties, note that some of these values were taken from Aspen HYSYS
 T0 = 470; % units of K
-P0 = 1000; % units of kPa
+P0 = 1500; % units of kPa
 D = 0.0245; % units of m; diameter of tube
-L = 10;  % units of m
-N = 2000; % number of tubes
+L = 5.5;  % units of m
+N = 1100; % number of tubes
 Ac = (pi*((D^2)/4)); % units of m^2
 phi = 0.4; % represents the void fraction
 Dp = D/8; % units of m; diameter of particle
 mu = 2.11*10^-5; % units of kg/m*s
 rho0 = 8.239; % units of kg/m^3
 V_r = (pi*((D^2)/4))*L; % units of m^3
-Vdot0 = 9581/3600/100; % units of m^3/s
 
 %Coolant Properties
 U = 0.3; % units of kJ/(m^2*K*s)
-Tc0 =430; % units of K, boiling point is 530 K
+Tc0 =410; % units of K, boiling point is 530 K
 flowC = 3010/3600; % units of kg/s
 
 
@@ -58,19 +58,20 @@ F5_0 = 0/3600; % 5 = co2
 F6_0 = 0.1/3600; % 6 = cl2
 F7_0 = 0/3600; % 7 = 1,2-dichloroethane
 F8_0 = 1/3600; % 8 = h2o
-Ftotal_0 = F1_0 + F2_0 + F3_0 + F4_0 + F5_0 + F6_0 + F7_0 + F8_0;
-    
+F = [F1_0 F2_0 F3_0 F4_0 F5_0 F6_0 F7_0 F8_0];     
+Ftotal_0 = sum(F);
 
 %Ergun Equation Parameters
-G = rho0*(Vdot0/Ac); % units of kg/(m^2 * s)
+MW = [0.02805, 0.03646, 0.01600, 0.1334, 0.04401, 0.0709, 0.09896, 0.01802]; %kg/mol
+G = sum(MW.*F)/Ac; % units of kg/(m^2 * s)
 Beta = (((G/(Dp)) * ((1-phi)/(phi^3))) * (((150*(1-phi)*mu)/Dp) + (1.75*G)))/1000; % units of kPa*kg/(m^4)
 
 %Logic
-numElements = 200; % number of solver iterations
+numElements = 1000; % number of solver iterations
 dv = V_r/numElements;
 vspan = linspace(0, V_r, numElements);
 y0 = [F1_0 F2_0 F3_0 F4_0 F5_0 F6_0 F7_0 F8_0 T0 P0 Tc0]; % load dependent variables
-handleranon = @(v,y) handler(v,y,phi,H_tot,Cp_tot,L,D,Beta,Dp,Ac,N,U,flowC,Ftotal_0,T0,P0,rho0,dv); % use handler fxn
+handleranon = @(v,y) handler(v,y,phi,H_tot,Cp_tot,L,D,Beta,Ac,U,flowC,Ftotal_0,T0,P0,rho0); % use handler fxn
 [ v, ysoln ] = ode15s(handleranon,vspan,y0);
 conv = zeros(numElements,1);
 for i = 1:numElements
